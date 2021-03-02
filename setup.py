a43dd51e8c098ec89c79fff28a72282913b45366
Justin Lakkis <jlakkis@Justins-MacBook-Pro.local>
2021-03-01 23:58:25 -0500

    relax dependencies

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cardec",
-    version="1.0.2.4",
+    version="1.0.2.5",
     author="Justin Lakkis",
     author_email="jlakks@gmail.com",
     description="A deep learning method for joint batch correction, denoting, and clustering of single-cell rna-seq data.",
@@ -23,6 +23,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['numpy==1.18.1', 'pandas==1.0.1', 'scipy==1.4.1', 'tensorflow>= 2.0.1, <=2.3.1', 'scikit-learn==0.22.2.post1', 'scanpy==1.5.1', 'louvain==0.6.1'],
+    install_requires=['numpy>=1.18.1', 'pandas>=1.0.1', 'scipy>=1.4.1', 'tensorflow>= 2.0.1, <=2.3.1', 'scikit-learn>=0.22.2.post1', 'scanpy>=1.5.1', 'louvain>=0.6.1'],
     python_requires='>=3.7',
 )
